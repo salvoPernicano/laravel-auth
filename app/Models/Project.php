841a25e307de9537e65b8e52de1a925bcdf525d5
salvoPernicano <salvo.pernicano@gmail.com>
2024-03-25 20:17:24 +0100

    creazione migration / model / seeder / route in auth

---
+++ app/Models/Project.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nome_progetto',
+        'slug',
+        'descrizione_progetto',
+        'linguaggi',
+        'immagine'
+    ];
+
+    public static function generateSlug($nome_progetto){
+        return Str::slug($nome_progetto, '-');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,4 +29,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::middleware('auth')
+->prefix('dashboard')
+->name('dashboard.')
+->group(function(){
+
+        Route::resource('projects', ProjectController::class);
+});
 require __DIR__.'/auth.php';
