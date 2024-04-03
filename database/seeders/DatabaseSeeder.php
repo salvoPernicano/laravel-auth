7f983120d11dc52a732c63bef30e656fa6def705
salvoPernicano <salvo.pernicano@gmail.com>
2024-04-03 17:44:05 +0200

    definizione relazioni, creazione modello,migration,seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Technology;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,7 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             ProjectSeeder::class,
-            TypeSeeder::class
+            TypeSeeder::class,
+            TechnologySeeder::class
         ]);
     }
 }
